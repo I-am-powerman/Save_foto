07366e7c8a230a3946f015a32f3a8d5f1b034c42
I-am-powerman <seredkin.dmitrii.powerman@gmail.com>
2025-06-04 19:32:39 +0800

    Дописал при помощи дипсика, но разбирался в каждой строчке

--- myenv/bin/activate.fish
+++ myenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/dmitrii/Документы/Proekti/Project_throwing_photos/myenv'
+set -gx VIRTUAL_ENV '/home/dmitrii/Документы/Proekti/Save_foto/myenv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
